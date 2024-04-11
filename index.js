4cf0cd0acdcdefd1a3b786b436644196e7e40fd8
kushwahaPrashant24 <146343202+kushwahaPrashant24@users.noreply.github.com>
2024-04-11 12:12:59 +0530

    added the project

---
+++ index.js
@@ -0,0 +1,36 @@
+import  inputText from "inquirer";
+import qr from "qr-image"
+import fs from "fs";
+import { v4 as uuidv4 } from 'uuid';
+
+
+
+
+
+
+inputText
+.prompt([
+    {
+        message: "Type URL Here",
+        name: "URL",
+    }
+  ])
+
+  .then((answers) => {
+     const url = answers.URL;
+     var qr_svg = qr.image(url);
+     qr_svg.pipe(fs.createWriteStream(`qr_${uuidv4()}.png`));
+
+     fs.writeFile("URL.txt", url, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+     })
+  })
+
+  .catch((error) => {
+    if (error.isTtyError) {
+      
+    } else {
+      
+    }
+  });
